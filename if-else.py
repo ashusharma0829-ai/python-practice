2016f56c5fc5316219faaadd9577cd962b0e0a77
ashusharma0829-ai <ashusharma0829@gmail.com>
2026-03-27 16:30:59 +0530

    if-else.py

---
+++ if-else.py
@@ -0,0 +1,11 @@
+#traffic light
+#Take input from user
+color = input("Enter Your traffic light : ")
+if(color == 'Red'):
+    print("Stop")
+elif(color == 'Green'):
+    print("Go")
+elif(color == 'Yellow'):
+    print("Look")
+else:
+    print("Broken Light")    
